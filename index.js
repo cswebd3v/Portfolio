8332e3b7285450558a427c2d315e5674997b6008
Chris Swenson <cswebd3v@gmail.com>
2019-09-08 19:01:22 -0500

    updated all files and added img

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.slide1').fadeIn(3000).removeClass('hidden');
+});
+
+$(document).ready(function () {
+    $('.slide2').fadeIn(4900).delay(2000).removeClass('hidden');
+});
+
